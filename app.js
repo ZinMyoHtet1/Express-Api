1c19cfa03218fc9ad45e2d436f343d93f6471314
ZinMyoHtet1 <zmhtetcompany@gmail.com>
2025-05-26 18:26:48 +0630

    add userModel.js

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
 const express = require("express");
-const path= require("path");
-const morgan=require("morgan")
+const path = require("path");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
-const CustomError=require("./utils/CustomError.js")
-dotenv.config({path: "./config.env"})
+const CustomError = require("./utils/CustomError.js");
+dotenv.config({ path: "./config.env" });
 
 require("./helpers/mongoose_init.js");
 
-const phoneRoutes=require("./routes/phoneRoutes.js")
+const phoneRoutes = require("./routes/phoneRoutes.js");
 
-const {getPhoneStats}=require("./controllers/phoneController.js");
+const { getPhoneStats } = require("./controllers/phoneController.js");
 
-const globalErrorHandler=require("./utils/globalErrorHandler.js")
+const globalErrorHandler = require("./utils/globalErrorHandler.js");
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
-process.on("unhandleRejection",function(){
-    console.log("UnhandleRejection Occuring...")
-    server.close(()=>process.exit(1))
+process.on("unhandleRejection", function () {
+  console.log("UnhandleRejection Occuring...");
+  server.close(() => process.exit(1));
 });
 
-if(process.env.MODE=== "development"){
-    app.use(morgan("dev"))
+if (process.env.MODE === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.static("./public"));
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname+"/Templates/index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/Templates/index.html"));
+});
 
-app.get("/api/v1", (req, res)=> {
-    res.status(200).send("Hi! Welcome@back")
-})
+app.get("/api/v1", (req, res) => {
+  res.status(200).send("Hi! Welcome@back");
+});
 
-app.get("/api/v1/phone-stats",getPhoneStats)
+app.get("/api/v1/phone-stats", getPhoneStats);
 
-app.use("/api/v1/phones",phoneRoutes)
+app.use("/api/v1/phones", phoneRoutes);
 
-app.all('/{*any}',(req,res,next)=>{
-    
-    const error=new CustomError(`Route "${req.originalUrl} is not found`, 404)
-    next(error)
-})
+app.all("/{*any}", (req, res, next) => {
+  const error = new CustomError(`Route "${req.originalUrl} is not found`, 404);
+  next(error);
+});
 
 //GLOBAL ERROR HANDLING
 app.use(globalErrorHandler);
 
-const server=app.listen(port, ()=> {
-    console.log(`Your server is running on port ${port}`);
-})
+const server = app.listen(port, () => {
+  console.log(`Your server is running on port ${port}`);
+});
--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,66 +1,69 @@
-const CustomError =require("./CustomError.js");
+const CustomError = require("./CustomError.js");
 
-function castErrorHandler(err){
-    const msg=`Invalid value for field ${err.path}: ${err.value}`
-    return new CustomError(msg,400)
+function castErrorHandler(err) {
+  const msg = `Invalid value for field ${err.path}: ${err.value}`;
+  return new CustomError(msg, 400);
 }
 
-function duplicateKeyErrorHandler(err){
-    const msg=`There already had phone name. Please try another name`
-    return new CustomError(msg,400)
-    
-};
-
-function validationErrorHandler(err){
-    const contents= Object.values(err.errors).map(val=> val).join(". ")
-    const msg=`Validation Failed: ${contents}`
-    return new CustomError(msg,400)
+function duplicateKeyErrorHandler(err) {
+  const msg = `There already had phone name. Please try another name`;
+  return new CustomError(msg, 400);
 }
 
-function referenceErrorHandler(err){
-    const msg=`ReferenceError : ${err.message}`
-    return new CustomError(msg,400)
+function validationErrorHandler(err) {
+  const contents = Object.values(err.errors)
+    .map((val) => val)
+    .join(". ");
+  const msg = `Validation Failed: ${contents}`;
+  return new CustomError(msg, 400);
 }
 
-function devError(res,error) {
-    console.log(error?.name);
+function referenceErrorHandler(err) {
+  const msg = `ReferenceError : ${err.message}`;
+  return new CustomError(msg, 400);
+}
+
+function devError(res, error) {
+  console.log(error?.name);
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stackTrace: error.stack,
+    error: error,
+  });
+}
+
+function proError(res, error) {
+  if (error.isOperational) {
     res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-        stackTrace: error.stack,
-        error: error
-    })
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong! Try again later.",
+    });
+  }
+}
+
+module.exports = function (error, req, res, next) {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "error";
+
+  if (process.env.MODE === "development") {
+    devError(res, error);
+  } else if (process.env.MODE === "production") {
+    if (error?.name === "CastError") error = castErrorHandler(error);
+
+    if (error?.errorResponse?.code === 11000)
+      error = duplicateKeyErrorHandler(error);
+
+    console.log(error?.name);
+
+    if (error?.name === "ValidationError")
+      error = validationErrorHandler(error);
+    if (error?.name === "ReferenceError") error = referenceErrorHandler(error);
+    proError(res, error);
+  }
 };
-
-function proError(res,error) {
-    if (error.isOperational) {
-        res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message
-
-        })} else {
-        res.status(500).json({
-            status: "error",
-            message: "Something went wrong! Try again later."
-        })}
-}
-
-module.exports = function(error, req, res, next) {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || "error";
-    
-    if(process.env.MODE==="development"){
-        devError(res,error)
-        
-    }else if(process.env.MODE==="production"){
-        if(error?.name==="CastError") error =castErrorHandler(error)
-        
-        if(error?.errorResponse?.code===11000) error =duplicateKeyErrorHandler(error)
-        
-        console.log(error?.name)
-        
-        if(error?.name==="ValidationError") error=validationErrorHandler(error)
-        if(error?.name==="ReferenceError") error= referenceErrorHandler(error)
-        proError(res,error)
-    }
-}
